87ff3a9a699c13236bde8ab30c79f65cd6b26a92
demetrio <dem.iaria@gmail.com>
2024-05-28 20:25:07 +0200

    Improved searh function, reducing Big-O Complexity

--- js/search.js
+++ js/search.js
@@ -5,20 +5,26 @@ function searchByArgumentLinkTopic() {
   var tr = tbody.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
-  for (let i = 1; i < tr.length; i++) {
-    for (let j = 0; j < 3; j++) {
-      let content = "";
-      content = tr[i].getElementsByTagName("td")[0].innerText;
-      content += tr[i].getElementsByTagName("td")[1].innerText;
-      content += tr[i].getElementsByTagName("td")[2].innerText;
+  for (let i = 0; i < tr.length; i++) {
 
-      if (content) {
-        if (content.toUpperCase().indexOf(wordToSearchFor.toUpperCase()) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
+    tr[i].style.display = "none";
+
+    let argument = "";
+    argument = tr[i].getElementsByTagName("td")[0].innerText;
+    if (argument.toUpperCase().indexOf(wordToSearchFor) > -1) {
+      tr[i].style.display = "";
+    }
+
+    let link = "";
+    link = tr[i].getElementsByTagName("td")[1].innerText;
+    if (link.toUpperCase().indexOf(wordToSearchFor) > -1) {
+      tr[i].style.display = "";
+    }
+
+    let topic = "";
+    topic = tr[i].getElementsByTagName("td")[2].innerText;
+    if (topic.toUpperCase().indexOf(wordToSearchFor) > -1) {
+      tr[i].style.display = "";
     }
   }
 }
