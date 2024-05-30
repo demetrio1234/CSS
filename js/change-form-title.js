201d0031590d1f8f6659b4127cbc1b4269f13c90
demetrio <dem.iaria@gmail.com>
2024-05-30 21:43:34 +0200

    Added the input for the username in case of the new user registration

--- js/change-form-title.js
+++ js/change-form-title.js
@@ -5,8 +5,16 @@ function changeFormTitle() {
     if (cb_register) {
         document.querySelector('div[class="login-panel"]').style.backgroundColor = "aquamarine";
         document.querySelector('label[id="form-title"]').textContent = 'Register new User';
+        document.querySelector('input[name="username"]').style.display = 'block';
+        document.querySelector('input[name="repeated-password"]').style.display = 'block';
+        document.querySelector('input[name="remember-credentials"]').style.display = 'none';
+        document.querySelector('label[name="remember-credentials-label"]').style.display = "none";
     } else if (!cb_register) {
         document.querySelector('div[class="login-panel"]').style.backgroundColor = "white";
         document.querySelector('label[id="form-title"]').textContent = 'Login';
+        document.querySelector('input[name="username"]').style.display = 'none';
+        document.querySelector('input[name="repeated-password"]').style.display = 'none';
+        document.querySelector('input[name="remember-credentials"]').style.display = 'inline-block';
+        document.querySelector('label[name="remember-credentials-label"]').style.display = "inline-block";
     }
 }
